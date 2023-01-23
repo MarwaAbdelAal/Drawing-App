ea5b8d7e0d23a9eb7f455e3b838ebcc072c4c7bf
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2023-01-23 12:03:22 +0200

    all shapes done and erasing

--- js/script.js
+++ js/script.js
@@ -5,25 +5,29 @@ let line = document.querySelector('.line');
 let square = document.querySelector('.square');
 let circle = document.querySelector('.circle');
 let eraser = document.querySelector('.eraser');
+let reload = document.querySelector('.reload');
 let fillInput = document.getElementById('fill');
 let strokeInput = document.getElementById('stroke');
+let lineInput = document.getElementById('lineInput');
 
 line.addEventListener('click', () => mode = 'line');
 freeDraw.addEventListener('click', () => mode = 'freehand');
-square.addEventListener('click', drawSquare);
-circle.addEventListener('click', drawCircle);
-eraser.addEventListener('click', erase);
+square.addEventListener('click', () => mode = 'square');
+circle.addEventListener('click', () => mode = 'circle');
+eraser.addEventListener('click', () => mode = 'erase');
+reload.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height));
 fillInput.addEventListener('change', () => fillColor = fillInput.value);
 strokeInput.addEventListener('change', () => strokeColor = strokeInput.value);
+lineInput.addEventListener('change', () => strokeWidth = lineInput.value);
 
 let mode = 'line';
-let fillColor = 'black';
+let fillColor = 'white';
 let strokeColor = 'black';
+let strokeWidth = '2'; ////////////// need input for it
 
-let startPoint = { x: 0, y: 0 };
 let drawFlag = false;
-let sp = { x: 0, y: 0 }; //start point
-let ep = { x: 0, y: 0 }; //end point
+let startPoint = { x: 0, y: 0 }; //start point
+let endPoint = { x: 0, y: 0 }; //end point
 
 canvas.addEventListener('mousedown', (e) => {
     if (mode == 'line') {
@@ -34,9 +38,26 @@ canvas.addEventListener('mousedown', (e) => {
     }
     else if (mode == 'freehand') {
         ctx.beginPath();
-        sp.x = e.offsetX;
-        sp.y = e.offsetY;
-        ctx.moveTo(sp.x, sp.y);
+        startPoint.x = e.offsetX;
+        startPoint.y = e.offsetY;
+        ctx.moveTo(startPoint.x, startPoint.y);
+        drawFlag = true;
+    }
+    else if (mode == 'square') {
+        ctx.beginPath();
+        startPoint.x = e.offsetX;
+        startPoint.y = e.offsetY;
+    }
+    else if (mode == 'circle') {
+        ctx.beginPath();
+        startPoint.x = e.offsetX;
+        startPoint.y = e.offsetY;
+    }
+    else if (mode == 'erase') {
+        ctx.beginPath();
+        startPoint.x = e.offsetX;
+        startPoint.y = e.offsetY;
+        ctx.moveTo(startPoint.x, startPoint.y);
         drawFlag = true;
     }
 });
@@ -44,10 +65,21 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', (e) => {
     if (mode == 'freehand') {
         if (drawFlag) {
-            ep.x = e.offsetX;
-            ep.y = e.offsetY;
-            ctx.lineTo(ep.x, ep.y);
+            endPoint.x = e.offsetX;
+            endPoint.y = e.offsetY;
+            ctx.lineTo(endPoint.x, endPoint.y);
             ctx.strokeStyle = strokeColor;
+            ctx.lineWidth = strokeWidth;
+            ctx.stroke();
+        }
+    }
+    if (mode == 'erase') {
+        if (drawFlag) {
+            endPoint.x = e.offsetX;
+            endPoint.y = e.offsetY;
+            ctx.lineTo(endPoint.x, endPoint.y);
+            ctx.strokeStyle = 'white';
+            ctx.lineWidth = strokeWidth;
             ctx.stroke();
         }
     }
@@ -56,10 +88,35 @@ canvas.addEventListener('mousemove', (e) => {
 canvas.addEventListener('mouseup', (e) => {
     if (mode == 'line') {
         ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.lineWidth = strokeWidth;
         ctx.strokeStyle = strokeColor;
         ctx.stroke();
     } 
     else if (mode == 'freehand') {
+        drawFlag = false;
+    }
+    else if (mode == 'square') {
+        endPoint.x = e.offsetX;
+        endPoint.y = e.offsetY;
+        ctx.rect(startPoint.x, startPoint.y, endPoint.x - startPoint.x, endPoint.y - startPoint.y);
+        ctx.lineWidth = strokeWidth;
+        ctx.strokeStyle = strokeColor;
+        ctx.stroke();
+        ctx.fillStyle = fillColor;
+        ctx.fillRect(startPoint.x, startPoint.y, endPoint.x - startPoint.x, endPoint.y - startPoint.y);
+    }
+    else if (mode == 'circle') {
+        endPoint.x = e.offsetX;
+        endPoint.y = e.offsetY;
+        let rad = getDistance(startPoint.x, startPoint.y, endPoint.x, endPoint.y)
+        ctx.arc(startPoint.x, startPoint.y, rad, 0, 2 * Math.PI);  //(x,y,rad,start angle,end angle)
+        ctx.lineWidth = strokeWidth;
+        ctx.strokeStyle = strokeColor;
+        ctx.stroke();
+        ctx.fillStyle = fillColor;
+        ctx.fill();
+    }
+    else if (mode == 'erase') {
         drawFlag = false;
     }
 });
@@ -68,25 +125,15 @@ function drawSquare() {
     console.log('draw square');
     ctx.beginPath()
     ctx.rect(0,0,150,75)  //(x,y,width,height)
-    ctx.lineWidth="3"
-    // ctx.strokeStyle=strokeColor;
-    // ctx.stroke()
+    ctx.lineWidth = strokeWidth;
+    ctx.strokeStyle=strokeColor;
+    ctx.stroke()
     ctx.fillStyle=fillColor;
-    ctx.fillRect(150,150,150,75)
+    ctx.fillRect(0,0,150,75)
 }
 
-function drawCircle() {
-    console.log('draw circle');
-}
-
-function erase() {
-    console.log('eraser');
-}
-
-function fillStyle() {
-    strokeColor = fillInput.value;
-}
-
-function strokeStyle() {
-    console.log('stroke Style');
+function getDistance(x1, y1, x2, y2){
+    let y = x2 - x1;
+    let x = y2 - y1;    
+    return Math.sqrt(x * x + y * y);
 }
